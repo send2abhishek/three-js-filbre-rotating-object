3043d92ed7a6284041494b1388434d71a7acd280
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-07-15 16:25:35 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+
+import { Canvas, useFrame } from "react-three-fiber";
+import { softShadows, OrbitControls } from "drei";
+
+softShadows();
+
+const SpinningMesh = ({ position, args, color }) => {
+  const meshRef = useRef();
+
+  useFrame(
+    () => (meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01)
+  );
+
+  return (
+    <mesh castShadow position={position} ref={meshRef}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial attach="material" color={color} />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1}
+          shadowCameraFar={50}
+          shadowCameraLeft={-10}
+          shadowCameraRight={10}
+          shadowCameraTop={10}
+          shadowCameraBottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            args={[3, 2, 1]}
+            color="lightblue"
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="pink" />
+          <SpinningMesh position={[5, 1, -2]} color="pink" />
+        </group>
+
+        {/* <Box>
+          <meshStandardMaterial attach="material" />
+        </Box> */}
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 }
 
